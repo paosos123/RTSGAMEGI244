e5d139ef0c2ccd9cc0be16519a8f62c7bdf4cd9e
paosos123 <paosos123@gmail.com>
2024-02-15 10:59:02 +0700

    EndWeek6EndNumber2

--- Assets/Scripts/Buildings/Building.cs
+++ Assets/Scripts/Buildings/Building.cs
@@ -29,6 +29,8 @@ public class Building : Structure
     {
         if (Input.GetKeyDown(KeyCode.G))
             ToCreateUnit(0);
+        else if (Input.GetKeyDown(KeyCode.H))
+            ToCreateUnit(1);
 
         if ((recruitList.Count > 0) && (recruitList[0] != null))
         {
--- Assets/Scripts/Command/UnitSelect.cs
+++ Assets/Scripts/Command/UnitSelect.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.EventSystems;
 public class UnitSelect : MonoBehaviour
 {
     [SerializeField]
@@ -16,6 +16,9 @@ public class UnitSelect : MonoBehaviour
 
     public static UnitSelect instance;
 
+    [SerializeField]
+    private Building curBuilding; //current selected single building
+    public Building CurBuilding { get { return curBuilding; } }
 
     void Awake()
     {
@@ -38,6 +41,10 @@ public class UnitSelect : MonoBehaviour
 
         if (Input.GetMouseButtonDown(0))
         {
+            //if click UI dont Clear
+            if(EventSystem.current.IsPointerOverGameObject())
+                return;
+            
             ClearEverything();
         }
 
@@ -55,6 +62,11 @@ public class UnitSelect : MonoBehaviour
         curUnit.ToggleSelectionVisual(true);
 
         Debug.Log("Selected Unit");
+
+        if (GameManager.instance.MyFaction.IsMyUnit(curUnit))
+        {
+            ShowUnit(curUnit);
+        }
     }
 
     private void TrySelect(Vector2 screenPos)
@@ -70,6 +82,9 @@ public class UnitSelect : MonoBehaviour
                 case "Unit":
                     SelectUnit(hit);
                     break;
+                case "Building":
+                    BuildingSelect(hit);
+                    break;
             }
         }
     }
@@ -78,13 +93,37 @@ public class UnitSelect : MonoBehaviour
     {
         if (curUnit != null)
             curUnit.ToggleSelectionVisual(false);
+        if(curBuilding !=null)
+            curBuilding.ToggleSelectionVisual(false);
     }
 
     private void ClearEverything()
     {
         ClearAllSelectionVisual();
         curUnit = null;
+        curBuilding = null;
+        //ClearUI
+        InfoManager.instance.ClearAllInfo();
     }
+    private void ShowUnit(Unit u)
+    {
+        InfoManager.instance.ShowAllInfo(u);
+    }
+    private void ShowBuilding(Building b)
+    {
+        InfoManager.instance.ShowAllInfo(b);
+    }
+    private void BuildingSelect(RaycastHit hit)
+    {
+        curBuilding = hit.collider.GetComponent<Building>();
+        curBuilding.ToggleSelectionVisual(true);
 
+        if (GameManager.instance.MyFaction.IsMyBuilding(curBuilding))
+        {
+            //Debug.Log("my building");
+            ShowBuilding(curBuilding);//Show building info
+        }
+    }
+    
 
 }
