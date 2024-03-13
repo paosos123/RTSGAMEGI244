1254d3081d4a07e52596544ea319b83527cbeedf
paosos123 <paosos123@gmail.com>
2024-03-13 15:11:01 +0700

    end14
    end14.9

--- Assets/Scripts/Units/Unit.cs
+++ Assets/Scripts/Units/Unit.cs
@@ -31,6 +31,11 @@ public class Unit : MonoBehaviour
     private NavMeshAgent navAgent;
     public NavMeshAgent NavAgent { get { return navAgent; } }
     [SerializeField] private Faction faction;
+    public Faction Faction
+    {
+        get { return faction; }
+        set { faction = value; }
+    }
     [SerializeField] private GameObject selectionVisual;
     public GameObject SelectionVisual { get { return selectionVisual; } }
     [SerializeField] private UnitCost unitCost;
@@ -40,6 +45,13 @@ public class Unit : MonoBehaviour
     [SerializeField] private float unitWaitTime = 0.1f;
     public float UnitWaitTime { get { return unitWaitTime; } }
     
+    [SerializeField] private bool isBuilder;
+    public bool IsBuilder { get { return isBuilder; } set { isBuilder = value; } }
+
+    [SerializeField] private Builder builder;
+    public Builder Builder { get { return builder; } }
+
+
    
 
     void Awake()
