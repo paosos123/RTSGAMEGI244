232ee2c46ec805effd719470b49048753ad4ed6c
paosos123 <paosos123@gmail.com>
2024-03-20 16:41:25 +0700

    End16.26

--- Assets/Scripts/Units/Unit.cs
+++ Assets/Scripts/Units/Unit.cs
@@ -47,6 +47,11 @@ public class Unit : MonoBehaviour
     [SerializeField] private Builder builder;
     public Builder Builder { get { return builder; } }
 
+    [SerializeField] private bool isWorker;
+    public bool IsWorker { get { return isWorker; } set { isWorker = value; } }
+
+    [SerializeField] private Worker worker;
+    public Worker Worker { get { return worker; } }
 
    
 
@@ -56,6 +61,8 @@ public class Unit : MonoBehaviour
 
         if (isBuilder)
             builder = GetComponent<Builder>();
+        if (IsWorker)
+            worker = GetComponent<Worker>();
     }
 
     // Start is called before the first frame update
@@ -126,6 +133,10 @@ public enum UnitState
     Attack,
     MoveToBuild,
     BuildProgress,
+    MoveToResource,
+    Gather,
+    DeliverToHQ,
+    StoreAtHQ,
     Die
 }
 
